fca1a5bb662214569ded23d8f5218535713ac698
Geovane Fedrecheski <geonnave@gmail.com>
2024-10-23 14:38:40 +0200

    refactor: move config to own file, use class

--- swarmake/main.py
+++ swarmake/main.py
@@ -7,69 +7,27 @@ import shutil
 import toml
 # import logging
 import sys
-from tqdm import tqdm
-from rich.console import Console
-from rich.logging import RichHandler
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from itertools import cycle
+from typing import List, Dict
 import time
 
-# import log
 from swarmake.logger import setup_logging, LOGGER
 logging = LOGGER.bind(context=__name__)
 
 from swarmake.run_cmd import execute_command
-
-# Load swarmake.toml configuration
-BASE_CONFIG_FILE = "swarmake.toml"
-config = {}
-
-console = Console()
-
-@dataclass
-class ProjectConfig:
-    name: str
-    repo_name: str
-    build_cmd: str = ""
-    setup_cmd: str = ""
-    run_cmd: str = ""
-    list_outputs_cmd: str = ""
-
-    @property
-    def build_dir(self):
-        return f"{config['_core']['build-dir']}/{self.name}"
-
-    @property
-    def url(self):
-        return f"{config['_core']['openswarm-url']}/{self.repo_name}"
+from swarmake.config import config
 
 def load_project_config(project_name):
     logging.debug(f"Loading project configuration", project_name=project_name)
-    project_key = project_name
-    if project_key not in config["projects"]:
+    
+    # Access the project from the config's project dictionary
+    if project_name not in config.projects:
         raise ValueError(f"Project '{project_name}' not found in configuration.")
-    
-    project_data = config["projects"][project_key]
 
-    repo_name = project_data.get("repo", project_name)  # If repo name is not explicitly defined, use project name
-    # make sure repo_name is in the list of repositories
-    if repo_name not in config["_core"]["repositories"]:
-        raise ValueError(f"Repository '{repo_name}' not found in configuration.")
-
-    build_cmd = project_data.get("build", "")
-    setup_cmd = project_data.get("setup", "")
-    run_cmd = project_data.get("run", "")
-    list_outputs_cmd = project_data.get("list-outputs", "")
-    
-    return ProjectConfig(
-        name=project_name,
-        repo_name=repo_name,
-        build_cmd=build_cmd,
-        setup_cmd=setup_cmd,
-        run_cmd=run_cmd,
-        list_outputs_cmd=list_outputs_cmd,
-    )
+    # Return the project directly
+    return config.projects[project_name]
 
 def clone_repository(url, destination):
     if not os.path.exists(destination):
@@ -80,7 +38,7 @@ def clone_repository(url, destination):
 
 def clean_build_dir(project_name=None):
     """Clean the build directory for the specified project."""
-    build_dir = f"{config['_core']['build-dir']}"
+    build_dir = config.core.build_dir
     if project_name:
         build_dir = f"{build_dir}/{project_name}"
         logging.info(f"Cleaning build directory for project", project_name=project_name, build_dir=build_dir)
@@ -95,9 +53,6 @@ def clean_build_dir(project_name=None):
 @click.group()
 @click.pass_context
 def main(ctx):
-    global config
-    config = toml.load(BASE_CONFIG_FILE)
-
     log_level = "info"
     passed_level = os.environ.get("PYTHON_LOG", "").lower()
     if passed_level in ["debug", "info", "warning", "error"]:
@@ -120,7 +75,7 @@ def build(project_name, clean_build_first):
     project = load_project_config(project_name)
 
     # Clone the repository if necessary
-    clone_repository(project.url, f"{config['_core']['build-dir']}/{project.name}")
+    clone_repository(project.url, project.build_dir)
 
     # Execute the setup and build commands
     os.chdir(project.build_dir)
@@ -143,7 +98,7 @@ def run(project_name, clean_build_first):
     project = load_project_config(project_name)
 
     # check if the project is cloned / built
-    if not os.path.exists(f"{config['_core']['build-dir']}/{project.name}"):
+    if not os.path.exists(f"{config.core.build_dir}/{project.name}"):
         raise ValueError(f"Project {project.name} has not been built. Please run 'swarmake build {project.name}' first.")
     
     # Execute the run command
@@ -158,18 +113,19 @@ def run(project_name, clean_build_first):
 def list():
     """List available projects."""
     configured_projects = []
-    for project_name in config["projects"]:
+    for project_name in config.projects:
         try:
             project = load_project_config(project_name)
             # filter only _cmd attributes
             commands = [k.replace("_cmd", "") for k, v in project.__dict__.items() if k.endswith("_cmd") and v != ""]
-            # logging.info(f"Found project {project_name}", commands=commands)
             configured_projects.append(project)
         except ValueError as e:
             logging.warning(e)
+
     configured_project_names = '\n\t'.join([p.name for p in configured_projects])
     logging.info(f"Found {len(configured_projects)} configured projects:\n\n\t{configured_project_names}\n\n")
-    all_repos = config["_core"]["repositories"]
+
+    all_repos = config.core.repositories
     unconfigured_projects = set(all_repos) - set([p.repo_name for p in configured_projects])
     unconfigured_project_names = '\n\t'.join(unconfigured_projects)
     logging.info(f"Found {len(unconfigured_projects)} unconfigured projects:\n\n\t{unconfigured_project_names}\n\n")
