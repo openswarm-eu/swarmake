eda05e81dfb3e64a51505f244c9253040304dfac
Geovane Fedrecheski <geonnave@gmail.com>
2024-10-23 09:57:03 +0200

    feat: can build and run several new projects

--- swarmake/main.py
+++ swarmake/main.py
@@ -45,6 +45,7 @@ class ProjectConfig:
         return f"{config['_swarmake']['openswarm-url']}/{self.repo_name}"
 
 def load_project_config(project_name):
+    logging.debug(f"Loading project configuration", project_name=project_name)
     project_key = project_name
     if project_key not in config["projects"]:
         raise ValueError(f"Project '{project_name}' not found in configuration.")
@@ -77,9 +78,14 @@ def clone_repository(url, destination):
     else:
         logging.info(f"Repository already cloned.", url=url, destination=destination)
 
-def clean_build_dir():
+def clean_build_dir(project_name=None):
     """Clean the build directory for the specified project."""
     build_dir = f"{config['_swarmake']['build-dir']}"
+    if project_name:
+        build_dir = f"{build_dir}/{project_name}"
+        logging.info(f"Cleaning build directory for project", project_name=project_name, build_dir=build_dir)
+    else:
+        logging.info(f"Cleaning the full build directory", build_dir=build_dir)
     if os.path.exists(build_dir):
         logging.info(f"Cleaning build directory", build_dir=build_dir)
         shutil.rmtree(build_dir)
@@ -106,7 +112,7 @@ def build(project_name, clean_build_first):
     print("================================================================================\n\n")
 
     if clean_build_first:
-        clean_build_dir()
+        clean_build_dir(project_name)
 
     project = load_project_config(project_name)
 
